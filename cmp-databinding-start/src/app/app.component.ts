332ed0ab60adbbaae959dcb2b2ee395f80e84986
DevPriti <mondal.18riya@gmail.com>
2018-12-25 16:09:12 +0530

    final commit

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -6,8 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{type: 'blueprint', name: 'TestServer', content: 'test the server!'},
-    {type: 'server', name: 'TestServer', content: 'for server testing'}];
+  serverElements = [{type: 'blueprint', name: 'TestServer', content: 'test the server!'}];
 
   onServerAdded(serverData: {serverName: string; serverContent: string}) {
     this.serverElements.push({
@@ -22,5 +21,12 @@ export class AppComponent {
       name: bluePrintData.serverName,
       content: bluePrintData.serverContent
     });
+  }
+  onFirstChange(){
+    this.serverElements[0].name = "Changed!!";
+    console.log("ngfirst change", this.serverElements[0].name);
+  }
+  onDestryFirst(){
+    this.serverElements.splice(0,1);
   }
 }
--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,8 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {
+  Component, Input, OnInit,
+  ViewEncapsulation, OnChanges,
+  SimpleChanges, DoCheck, OnDestroy, ViewChild, ElementRef, ContentChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,10 +10,44 @@ import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, OnDestroy {
   @Input('srvElement') element: {type: string; name: string; content: string};
-  constructor() { }
+  @Input() name: string;
+  @ViewChild('heading') heading: ElementRef;
+  @ContentChild('contentParagraph') paragraph :ElementRef;
+  constructor() {
+    console.log("Constructor calling!!");
 
+  }
+  ngOnChanges(changes : SimpleChanges){
+    console.log("NgOnChanges calling");
+    console.log("changes ", changes);
+
+  }
   ngOnInit() {
+    console.log("ngOnInit calling");
+    console.log('text content:' ,this.heading.nativeElement.textContent);
+    console.log('paragraph content:' ,this.paragraph.nativeElement.textContent);
+  }
+  ngDoCheck(): void {
+    console.log("NgDoChecked Calling!!");
+  }
+  ngAfterContentInit(){
+    console.log("ngAfterContentInIt calling!!");
+    console.log('paragraph content:' ,this.paragraph.nativeElement.textContent);
+  }
+  ngAfterContentChecked(){
+    console.log("NgAftercontentCheck Calling!!");
+  }
+
+  ngAfterViewInit(){
+    console.log("ngAfterViewInIt calling!!");
+    console.log('text content:' ,this.heading.nativeElement.textContent);
+  }
+  ngAfterViewChecked(){
+    console.log("ngAfterViewInIt Calling!!");
+  }
+  ngOnDestroy(): void {
+    console.log("NGONDESTRY")
   }
 }
