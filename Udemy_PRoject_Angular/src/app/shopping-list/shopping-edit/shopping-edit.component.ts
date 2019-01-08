1cb3987e5b0ce4024e1647e646cb797771cbf98c
DevPriti <44497068+DevPriti@users.noreply.github.com>
2019-01-08 14:47:55 +0530

    Update

--- Udemy_PRoject_Angular/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Udemy_PRoject_Angular/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
-import {Ingradients} from "../../shared/ingradient.model";
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Ingradients } from '../../shared/ingradient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,15 +10,16 @@ import {Ingradients} from "../../shared/ingradient.model";
 export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput') nameInputRef: ElementRef;
   @ViewChild('amountInput') amountInputRef: ElementRef;
-  @Output() newItem = new EventEmitter<Ingradients>();
-  constructor() { }
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
   }
+
   onAddItems() {
     const newIngradient = new
-    Ingradients(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value);
-    this.newItem.emit(newIngradient);
+      Ingradients(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value);
+    this.slService.addIngradients(newIngradient);
+    
   }
 
 }
