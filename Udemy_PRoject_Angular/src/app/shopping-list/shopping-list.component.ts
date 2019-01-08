739ca37d30ff056ac0fd28dcd16cba721aebc3b3
DevPriti <44497068+DevPriti@users.noreply.github.com>
2019-01-08 14:45:54 +0530

    Adding new ShoppingList Service

--- Udemy_PRoject_Angular/src/app/shopping-list/shopping-list.component.ts
+++ Udemy_PRoject_Angular/src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingradients } from "../shared/ingradient.model";
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,18 +8,16 @@ import { Ingradients } from "../shared/ingradient.model";
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingradient : Ingradients[] = [
-    new Ingradients('apples',5),
-    new Ingradients('eggs',4),
-    new Ingradients('milk',1),
-
-  ];
-  constructor() { }
+  ingradient: Ingradients[];
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
+    this.ingradient = this.slService.getIngradients();
+    this.slService.ingradientsChanged
+      .subscribe((newIngradient: Ingradients[]) => {
+        this.ingradient = newIngradient;
+      });
   }
-  onAddedIngradient(newaddedItem: Ingradients) {
-    this.ingradient.push(newaddedItem);
-  }
+  
 
 }
