d7f3685189453d0e00f6862a9f82ea3cc38e926b
DevPriti <44497068+DevPriti@users.noreply.github.com>
2019-01-08 14:53:29 +0530

    Update recipe-item.component.ts

--- Udemy_PRoject_Angular/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ Udemy_PRoject_Angular/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,6 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {Recipe} from "../../recipe.model";
-
+import { Component, OnInit , Input } from '@angular/core';
+import { Recipe } from "../../recipe.model";
+import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,14 +9,13 @@ import {Recipe} from "../../recipe.model";
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
-  @Output() recipeDetails = new EventEmitter<void>();
-  constructor() { }
+
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
   }
   showDetails() {
-    console.log("Details");
-    this.recipeDetails.emit();
+    this.recipeService.recipeSelected.emit(this.recipe);
   }
 
 }
