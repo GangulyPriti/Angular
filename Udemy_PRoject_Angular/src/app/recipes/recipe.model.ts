967b3b81c26761f448f4e5e252c2edc2044ef13e
DevPriti <44497068+DevPriti@users.noreply.github.com>
2019-01-08 14:51:44 +0530

    adding new ingredient parameter

--- Udemy_PRoject_Angular/src/app/recipes/recipe.model.ts
+++ Udemy_PRoject_Angular/src/app/recipes/recipe.model.ts
@@ -1,12 +1,15 @@
+import { Ingradients } from "../shared/ingradient.model";
 
 export class Recipe {
   public name: string;
   public description: string;
   public imagePath: string;
+  public ingradient: Ingradients[];
 
-  constructor(name: string, description: string, imagePath: string){
+  constructor(name: string, description: string, imagePath: string, ingradient: Ingradients[]) {
     this.name = name;
     this.description = description;
     this.imagePath = imagePath;
+    this.ingradient = ingradient;
   }
 }
