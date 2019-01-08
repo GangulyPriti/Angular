bda1b66b5c39b51a8921409fd4b45443a4cbe63f
DevPriti <44497068+DevPriti@users.noreply.github.com>
2019-01-08 19:38:10 +0530

    Update app.module.ts

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -11,13 +11,15 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import {RouterModule, Routes} from "@angular/router";
+import { Routes, RouterModule } from '@angular/router';
+ 
+const appRoutes: Routes = [
+  { path: 'users', component: UsersComponent },
+  { path: 'users/:id/:name', component: UserComponent },
+  { path: 'servers', component: ServersComponent },
+  { path: '', component: HomeComponent },
 
-const appRoutes : Routes = [
-  { path: '' , component: HomeComponent },
-  { path: 'users' , component: UsersComponent },
-  { path: 'servers' , component: ServersComponent },
-  ];
+];
 
 @NgModule({
   declarations: [
