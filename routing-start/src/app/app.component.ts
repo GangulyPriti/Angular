291fc4c19eab0818d5e6990f89e09167a76c484a
DevPriti <44497068+DevPriti@users.noreply.github.com>
2019-01-09 20:16:25 +0530

    Update app.component.ts

--- routing-start/src/app/app.component.ts
+++ routing-start/src/app/app.component.ts
@@ -2,12 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `{{course.money | currency : 'INR':true:'0.1-2' }}`,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  course = {
-    money : 5.324
-
-  }
 }
