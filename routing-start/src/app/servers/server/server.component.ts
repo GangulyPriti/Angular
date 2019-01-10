30d00379beff7dac1c739e0f8846b3fecae1b49a
DevPriti <44497068+DevPriti@users.noreply.github.com>
2019-01-10 14:31:02 +0530

    Update server.component.ts

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router, Data } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -18,13 +18,17 @@ export class ServerComponent implements OnInit {
     private router: Router) { }
 
   ngOnInit() {
-    const id = +this.route.snapshot.params['id'];
-    this.server = this.serversService.getServer(id);
-    this.paramSubscription = this.route.params
-        .subscribe(
-          (param: Params) => {
-            this.server = this.serversService.getServer(+param['id']);
-          });
+    this.route.data
+      .subscribe((data: Data) => {
+        this.server = data['server'];//to be match the name with app-routing resolver
+      });
+    //const id = +this.route.snapshot.params['id'];
+    //this.server = this.serversService.getServer(id);
+    //this.paramSubscription = this.route.params
+    //    .subscribe(
+    //      (param: Params) => {
+    //        this.server = this.serversService.getServer(+param['id']);
+    //      });
   }
   onEdit() {
     this.router.navigate(['edit'], { relativeTo: this.route, queryParamsHandling: 'preserve' });
