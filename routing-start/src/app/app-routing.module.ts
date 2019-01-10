76e84a26dfcaf348daea05ac06b0f6d60526bc50
DevPriti <44497068+DevPriti@users.noreply.github.com>
2019-01-10 14:29:21 +0530

    Update app-routing.module.ts

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -9,6 +9,8 @@ import { EditServerComponent } from "./servers/edit-server/edit-server.component
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { AuthGuard } from "./auth-guard.service";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate.service";
+import { ErrorPageComponent } from "./error-page/error-page.component";
+import { ServerResolver } from "./servers/server/server-resolver.service";
 
 
 const appRoutes: Routes = [
@@ -18,14 +20,16 @@ const appRoutes: Routes = [
     path: 'servers', component: ServersComponent,
     //canActivate: [AuthGuard],
     canActivateChild: [AuthGuard],
-    children: [{ path: ':id', component: ServerComponent },
+    children: [{ path: ':id', component: ServerComponent, resolve: { server: ServerResolver } },
     { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }]
   },
-  { path: 'not-found', component: PageNotFoundComponent },
+  //{ path: 'not-found', component: PageNotFoundComponent },
+  { path: 'not-found', component: ErrorPageComponent, data: {message:'Page Not Found!!'} },
   { path: '**', redirectTo: '/not-found' }
 ];
 @NgModule({
   imports: [
+    //RouterModule.forRoot(appRoutes, { useHash: true })
     RouterModule.forRoot(appRoutes)
   ],
   exports: [RouterModule]
